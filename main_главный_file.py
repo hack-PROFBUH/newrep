45e48598ae295974cf419f7c1d66e0e106a8a45d
Vadim Smirnov <98453684+VadimSmirnov213@users.noreply.github.com>
2023-06-25 07:28:21 +0300

    Update main_главный_file.py

--- main_главный_file.py
+++ main_главный_file.py
@@ -22,6 +22,14 @@ ys = yt.streams.get_highest_resolution()
 ys.download(filename="my_video.mp4")
 ###
 
+'''
+path = "my_video.mp4"                           # Для образки видео по таймкодам
+start_time = 5
+end_time = 15
+clip = VideoFileClip(path).subclip(start_time, end_time)
+clip.write_videofile("my_video_trimmed.mp4")
+'''
+
 ###
 video_path = "my_video.mp4"
 video = VideoFileClip(video_path)
